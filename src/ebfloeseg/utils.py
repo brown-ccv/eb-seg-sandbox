1c51a2d3d40d737a1a01c75d3a5b01d8e22918fb
cpaniaguam <cpaniaguam@gmail.com>
2024-06-17 23:27:05 -0400

    refactor: delete repeated code

--- src/ebfloeseg/utils.py
+++ src/ebfloeseg/utils.py
@@ -3,40 +3,6 @@ from datetime import datetime
 import numpy as np
 from numpy.typing import NDArray
 
-
-
-def mask_image(img: NDArray, mask: NDArray, val=0) -> NDArray:
-    img[mask] = val
-
-
-# test mask_image
-img = np.ones((5, 5))
-mask = np.array(
-    (
-        [
-            [True, False, True, False, False],
-            [False, False, True, True, False],
-            [False, True, False, True, True],
-            [False, False, True, True, False],
-            [False, True, True, True, False],
-        ]
-    )
-)
-mask_image(img, mask)
-arr = np.array(
-    (
-        [
-            [0.0, 1.0, 0.0, 1.0, 1.0],
-            [1.0, 1.0, 0.0, 0.0, 1.0],
-            [1.0, 0.0, 1.0, 0.0, 0.0],
-            [1.0, 1.0, 0.0, 0.0, 1.0],
-            [1.0, 0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-)
-assert np.allclose(img, arr)
-
-
 def getdoy(fname: str) -> str:
     return fname.split("_")[-2].zfill(3)
 
