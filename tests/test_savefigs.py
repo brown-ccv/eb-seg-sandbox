eed90a9ebae0742cf9634db3ce63ffc41ab38cbb
Carlos Paniagua <cpaniaguam@gmail.com>
2024-06-20 16:10:40 -0400

    feat: Add test for imsave function in test_savefigs.py

---
+++ tests/test_savefigs.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import rasterio
+import numpy as np
+
+from ebfloeseg.savefigs import imsave
+
+def test_imsave():
+    tci = rasterio.open("tests/input/tci/tci_2012-08-01_214_terra.tiff")
+
+    img = np.dstack(tci.read()[:])
+
+    temp = TemporaryDirectory()
+
+    # test without res provided
+    imsave(tci, img, Path(temp.name), "doy", "fname", count=3)
+    assert Path(temp.name).joinpath("fname").exists()
+
+    # test with res provided
+    imsave(tci, img, Path(temp.name), "doy", "fnameres", count=3, res="res")
+    assert Path(temp.name).joinpath("res_doy_fnameres").exists()
+
+    # test with as_uint8
+    imsave(tci, img[:,:, 1], Path(temp.name), "doy", "fnameuint8", count=1, as_uint8=True, rollaxis=False)
+    assert Path(temp.name).joinpath("fnameuint8").exists()
+
+    temp.cleanup()
