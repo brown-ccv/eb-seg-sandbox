c72d6e7124777211aafa8e43f80caf648f591b51
Carlos Paniagua <cpaniaguam@gmail.com>
2024-06-19 22:21:26 -0400

    test: peakdet

---
+++ tests/test_peakdet.py
@@ -0,0 +1,17 @@
+from ebfloeseg.peakdet import peakdet
+
+def test_peakdet():
+    v = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0]
+    delta = 1
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    peaks, valleys = peakdet(v, delta, x)
+    peaks = [tuple(x) for x in peaks]
+    valleys = [tuple(x) for x in valleys]
+    assert peaks == [(3, 2), (5, 3), (7, 4), (9, 5)]
+    assert valleys == [(4, 0), (6, 0), (8, 0)]
+
+    v = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0]
+    delta = 2
+    peaks, valleys = peakdet(v, delta)
+    assert [tuple(x) for x in peaks] == [(5,3), (7, 4), (9, 5)]
+    assert [tuple(x) for x in valleys]== [(6, 0), (8, 0)]
