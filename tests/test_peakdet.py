7dc95c797095f080a0f24e7fdf88ba2269aa9c58
Carlos Paniagua <cpaniaguam@gmail.com>
2024-06-20 15:06:56 -0400

    test: exception handling for peakdet function

--- tests/test_peakdet.py
+++ tests/test_peakdet.py
@@ -1,4 +1,5 @@
 from ebfloeseg.peakdet import peakdet
+import pytest
 
 
 def test_peakdet():
@@ -16,3 +17,20 @@ def test_peakdet():
     peaks, valleys = peakdet(v, delta)
     assert [tuple(x) for x in peaks] == [(5, 3), (7, 4), (9, 5)]
     assert [tuple(x) for x in valleys] == [(6, 0), (8, 0)]
+
+    # test with v and x of different lengths
+    v = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0]
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    with pytest.raises(ValueError):
+        peakdet(v, delta, x)
+
+    # test with non-scalar delta
+    delta = [1, 2]
+    with pytest.raises(ValueError):
+        peakdet(v, delta)
+
+    # test with negative delta
+    delta = -1
+    with pytest.raises(ValueError):
+        peakdet(v, delta)
+    
