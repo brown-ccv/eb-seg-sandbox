a2ed80abfffc9eb445df2ad1b6bad169f8e3e805
Carlos Paniagua <cpaniaguam@gmail.com>
2024-06-21 09:22:21 -0400

    refactor: Remove unnecessary x parameter in peakdet test

--- tests/test_peakdet.py
+++ tests/test_peakdet.py
@@ -5,8 +5,7 @@ import pytest
 def test_peakdet():
     v = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0]
     delta = 1
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    peaks, valleys = peakdet(v, delta, x)
+    peaks, valleys = peakdet(v, delta)
     peaks = [tuple(x) for x in peaks]
     valleys = [tuple(x) for x in valleys]
     assert peaks == [(3, 2), (5, 3), (7, 4), (9, 5)]
@@ -17,12 +16,6 @@ def test_peakdet():
     peaks, valleys = peakdet(v, delta)
     assert [tuple(x) for x in peaks] == [(5, 3), (7, 4), (9, 5)]
     assert [tuple(x) for x in valleys] == [(6, 0), (8, 0)]
-
-    # test with v and x of different lengths
-    v = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0]
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    with pytest.raises(ValueError):
-        peakdet(v, delta, x)
 
     # test with non-scalar delta
     delta = [1, 2]
