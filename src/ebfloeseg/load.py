aa38ca33b0056c332b893168fd69bacc50448dc6
John Gerrard Holland <john_holland1@brown.edu>
2024-07-31 18:55:16 +0000

    add data load function

--- src/ebfloeseg/load.py
+++ src/ebfloeseg/load.py
@@ -1,42 +1,49 @@
+from enum import Enum
+from pathlib import Path
+from typing import Annotated
 import requests
-import json
-import configparser
 
-import numpy as np
-import csv
-import math
-import pandas as pd
-import os, sys
+import typer
 
 
-from datetime import datetime
-import rasterio
-import warnings
-warnings.filterwarnings('ignore')
-import requests
-import netCDF4 as nc
+class ImageType(str, Enum):
+    truecolor = "truecolor"
+    cloud = "cloud"
 
 
-res='2016-07-01'
-doy = 1
-
-#download cloud data
-#url = 'https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME='+res+'T00:00:00Z&BBOX=-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486&CRS=EPSG:3413&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor,MODIS_Terra_Cloud_Fraction_Day&WRAP=day,day&FORMAT=image/tiff&WIDTH=2414&HEIGHT=4184&ts=1678819190554'
-
-#download TC data:
-url = 'https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME='+res+'T00:00:00Z&BBOX=-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486&CRS=EPSG:3413&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor&WRAP=day&FORMAT=image/tiff&WIDTH=4712&HEIGHT=4579&ts=1683675557694'
-
-r = requests.get(url, allow_redirects=True)
-f_direc='./data/'
-fname='tci_'+res+'_'+str(doy)+'_terra.tiff'
-open(f_direc+fname, 'wb').write(r.content)
+app = typer.Typer()
 
 
+@app.command()
+def main(
+    outfile: Annotated[Path, typer.Argument()],
+    date: str = "2016-07-01",
+    time: str = "T00:00:00Z",
+    wrap: str="day",
+    kind: ImageType = ImageType.truecolor,
+    bbox: str = "-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486",
+    crs: str = "EPSG:3413",
+    ts: int = 1683675557694,
+    width: int = 4712,
+    height: int = 4579,
+    format: str= "image/tiff",
+):
 
-res='2016-07-01'
-url = 'https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME='+res+'T00:00:00Z&BBOX=-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486&CRS=EPSG:3413&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor,MODIS_Terra_Cloud_Fraction_Day&WRAP=day,day&FORMAT=image/tiff&&WIDTH=4712&HEIGHT=4579&ts=1683675557694'
+    match kind:
+        case ImageType.truecolor:
+            layers = "MODIS_Terra_CorrectedReflectance_TrueColor"
+        case ImageType.cloud:
+            layers = "MODIS_Terra_CorrectedReflectance_TrueColor,MODIS_Terra_Cloud_Fraction_Day"
 
-r = requests.get(url, allow_redirects=True)
-f_direc='./data/'
-fname='cloud_'+res+'_'+str(doy)+'_terra.tiff'
-open(f_direc+fname, 'wb').write(r.content)
+    url = f"https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME={date}{time}&BBOX={bbox}&CRS={crs}&LAYERS={layers}&WRAP={wrap}&FORMAT={format}&WIDTH={width}&HEIGHT={height}&ts={ts}"
+
+    r = requests.get(url, allow_redirects=True)
+    outfile.parent.mkdir(parents=True, exist_ok=True)
+    with open(outfile, "wb") as f:
+        f.write(r.content)
+
+    return
+
+
+if __name__ == "__main__":
+    app()
