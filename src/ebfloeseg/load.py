b4147a2be19b154789a3a335fc19ba0d8c68c730
John Gerrard Holland <john_holland1@brown.edu>
2024-07-31 19:04:31 +0000

    simplify datetime

--- src/ebfloeseg/load.py
+++ src/ebfloeseg/load.py
@@ -17,16 +17,15 @@ app = typer.Typer()
 @app.command()
 def main(
     outfile: Annotated[Path, typer.Argument()],
-    date: str = "2016-07-01",
-    time: str = "T00:00:00Z",
-    wrap: str="day",
+    datetime: str = "2016-07-01T00:00:00Z",
+    wrap: str = "day",
     kind: ImageType = ImageType.truecolor,
     bbox: str = "-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486",
     crs: str = "EPSG:3413",
     ts: int = 1683675557694,
     width: int = 4712,
     height: int = 4579,
-    format: str= "image/tiff",
+    format: str = "image/tiff",
 ):
 
     match kind:
@@ -35,7 +34,7 @@ def main(
         case ImageType.cloud:
             layers = "MODIS_Terra_CorrectedReflectance_TrueColor,MODIS_Terra_Cloud_Fraction_Day"
 
-    url = f"https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME={date}{time}&BBOX={bbox}&CRS={crs}&LAYERS={layers}&WRAP={wrap}&FORMAT={format}&WIDTH={width}&HEIGHT={height}&ts={ts}"
+    url = f"https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME={datetime}&BBOX={bbox}&CRS={crs}&LAYERS={layers}&WRAP={wrap}&FORMAT={format}&WIDTH={width}&HEIGHT={height}&ts={ts}"
 
     r = requests.get(url, allow_redirects=True)
     outfile.parent.mkdir(parents=True, exist_ok=True)
