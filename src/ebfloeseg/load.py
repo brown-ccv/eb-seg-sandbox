262006b76c76083c0d155ed6ed4837fe145ce926
John Gerrard Holland <john_holland1@brown.edu>
2024-08-01 17:52:08 +0000

    add function to set width and height of the image based on the bounding box and a maximum size

--- src/ebfloeseg/load.py
+++ src/ebfloeseg/load.py
@@ -11,6 +11,28 @@ class ImageType(str, Enum):
     cloud = "cloud"
     landmask = "landmask"
 
+def get_width_height(bbox: str, size: int):
+    """Get width and height for a bounding box where the longer axis has length `size`
+    
+    Examples:
+        >>> get_width_height("0,0,1,1", 10)
+        (10, 10)
+
+        >>> get_width_height("0,0,1,5", 10)
+        (2, 10)
+
+        >>> get_width_height("0,0,1,5", 10)
+        (2, 10)
+
+    """
+    x1, y1, x2, y2 = [float(n) for n in bbox.split(",")]
+    x_size = abs(x2 - x1)
+    y_size = abs(y2 - y1)
+
+    max_size = max(x_size, y_size)
+
+    width, height = int(size * x_size / max_size), int(size * y_size / max_size)
+    return width, height
 
 app = typer.Typer()
 
@@ -22,8 +44,7 @@ def main(
     wrap: str = "day",
     kind: ImageType = ImageType.truecolor,
     bbox: str = "-2334051.0214676396,-414387.78951688844,-1127689.8419350237,757861.8364224486",
-    width: int = 4712,
-    height: int = 4579,
+    size: int = 4096,
     crs: str = "EPSG:3413",
     ts: int = 1683675557694,
     format: str = "image/tiff",
@@ -37,7 +58,9 @@ def main(
         case ImageType.landmask:
             layers = "OSM_Land_Mask"
 
-    
+
+    width, height = get_width_height(bbox, size)
+    print(width, height)
 
     url = f"https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME={datetime}&BBOX={bbox}&CRS={crs}&LAYERS={layers}&WRAP={wrap}&FORMAT={format}&WIDTH={width}&HEIGHT={height}&ts={ts}"
 
